e219a2092d2376c9da2a4300b701e3b392cc6961
FeiShao <robinshao007@163.com>
2026-08-05 11:05:16 +0800

    完善monitor功能，输出到日志文件并同步控制台打印

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -12,6 +12,7 @@
 #include <linux/types.h>
 #include <spdlog/spdlog.h>
 #include <unordered_map>
+#include <sstream>
 
 // 包含生成的skeleton头文件
 #include "../bpf/lsm_file.skel.h"
@@ -46,7 +47,6 @@ void init_inode_maps(const Config &config) {
 }
 
 static int handle_event(void *ctx, void *data, size_t data_sz) {
-    std::cout <<"-------------------------------------------"<<endl;
     auto *e = static_cast<struct event *>(data);
 
     auto it_path = g_inode_to_path.find(e->ino);
@@ -57,22 +57,29 @@ static int handle_event(void *ctx, void *data, size_t data_sz) {
 
     auto it_hash = g_inode_to_hash.find(e->ino);
     if (it_hash == g_inode_to_hash.end()) {
-        std::cout << "["<< actionToString(static_cast<Action>(e->action)) <<"] File access detected (no hash baseline)\n"
+
+        
+        std::ostringstream oss;
+        oss << "["<< actionToString(static_cast<Action>(e->action)) <<"] File access detected (no hash baseline)\n"
                   << "  path: " << path << "\n"
                   << "  pid: " << e->pid << "\n"
                   << "  comm: " << e->comm << std::endl;
+        spdlog::warn(oss.str());
+        spdlog::default_logger()->flush(); 
         return 0;
     }
     const std::string &expected_hash = it_hash->second;
     std::string current_hash = compute_sha256(path);
 
     if (current_hash != expected_hash) {
-        std::cout << "["<< e->action <<"] File modified! hash mismatch\n"
+        std::ostringstream oss;
+        oss << "["<< actionToString(static_cast<Action>(e->action)) <<"] File modified! hash mismatch\n"
                   << "  path: " << path << "\n"
                   << "  pid: " << e->pid << "\n"
                   << "  comm: " << e->comm << "\n"
                   << "  expected_hash: " << expected_hash << "\n"
                   << "  current_hash:  " << current_hash << std::endl;
+        spdlog::warn(oss.str());
     }
 
     return 0;
@@ -126,7 +133,7 @@ int do_monitor(const Config &config) {
     // 初始化 inode 映射
     init_inode_maps(config);
 
-    std::cout << "Monitoring started. Press Ctrl+C to stop." << std::endl;
+    spdlog::info("Monitoring started. Press Ctrl+C to stop.");
 
     struct ring_buffer *rb =
         ring_buffer__new(bpf_map__fd(skel->maps.rb), handle_event, nullptr, nullptr);
@@ -148,7 +155,7 @@ int do_monitor(const Config &config) {
     }
 
     spdlog::info("[service_stop] monitoring loop exited");
-    std::cout << "Monitoring stopped." << std::endl;
+    spdlog::info("Monitoring stopped.");
     ring_buffer__free(rb);
     lsm_file_bpf__destroy(skel);
     return 0;
